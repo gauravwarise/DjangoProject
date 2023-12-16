1669a0fd6ed0963647167131194e74ad54358a46
gaurav warise <gauravwarise04@gmail.com>
2023-12-16 13:46:12 +0530

    merge with main

--- djangoproject/static/js/authToggle.js
+++ djangoproject/static/js/authToggle.js
@@ -1,5 +1,3 @@
-// authToggle.js
-
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('toggleSignUp').addEventListener('click', function () {
         document.getElementById('authForm').innerHTML = `
@@ -22,9 +20,17 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
 
             <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-lg btn-block">Sign Up</button>
+            <button type="submit" class="btn btn-primary btn-lg btn-block" id="submitButton">Sign Up</button>
             <button type="button" class="btn btn-primary btn-lg btn-block" id="toggleSignIn">Sign In</button>
+            
+            <!-- Move the event listener registration inside the 'toggleSignUp' click event -->
+            <button type="button" class="btn btn-link" id="toggleForm">Forgot password?</button>
         `;
+        
+        // Move the event listener registration inside the 'toggleSignUp' click event
+        document.getElementById('toggleForm').addEventListener('click', function () {
+            // Handle Forgot Password
+        });
     });
 
     document.getElementById('toggleSignIn').addEventListener('click', function () {
@@ -51,12 +57,65 @@ document.addEventListener('DOMContentLoaded', function () {
             </div>
 
             <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+            <button type="submit" class="btn btn-primary btn-lg btn-block" id="submitButton">Sign in</button>
             <button type="button" class="btn btn-primary btn-lg btn-block" id="toggleSignUp">Sign Up</button>
+
+            <!-- Move the event listener registration inside the 'toggleSignIn' click event -->
+            <button type="button" class="btn btn-link" id="toggleForm">Forgot password?</button>
         `;
+        
+        // Move the event listener registration inside the 'toggleSignIn' click event
+        document.getElementById('toggleForm').addEventListener('click', function () {
+            // Handle Forgot Password
+        });
     });
 
     document.getElementById('toggleForm').addEventListener('click', function () {
         // Handle Forgot Password
     });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // ... your existing code ...
+
+    document.getElementById('submitButton').addEventListener('click', function () {
+        // Get values from the form
+        console.log("clicked works");
+        const email = document.getElementById('form1Example13').value;
+        const password = document.getElementById('form1Example23').value;
+
+        // Validate the inputs (you might want to add more validation)
+        if (!email || !password) {
+            alert('Please enter both email and password');
+            return;
+        }
+
+        // Create a data object to send in the request
+        const data = {
+            email: email,
+            password: password
+        };
+
+        // Send a POST request to your backend endpoint for signup
+        fetch('/account/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the server (success or error)
+            console.log('Success:', data);
+            // You might want to redirect the user to a login page or display a success message
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            // Handle the error (e.g., display an error message to the user)
+        });
+    });
+
+    // ... rest of your existing code ...
 });
